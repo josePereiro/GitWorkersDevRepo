6d0ccea5a38fc4b128e6a5199b42219a6eb35874
josePereiro <jose.pereiro.lab@gmail.com>
2020-07-14 12:01:48 -0400

    Master to Worker1: shut up!!!

--- Worker1/REPL/origin/repl.jl
+++ Worker1/REPL/origin/repl.jl
@@ -1,1 +1,12 @@
-println("Working version Baby!!!!!")
+try
+    using Random
+catch err
+    import Pkg
+    Pkg.add("Random")
+    using Random
+end
+
+for i in 1:100
+    println(randstring(i))
+    sleep(rand())
+end
